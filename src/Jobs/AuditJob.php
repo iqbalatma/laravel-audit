fb1bf1e33118afa4c6b66fcb48cfe6f01cc9d07f
Iqbal Atma Muliawan <35129050+iqbalatma@users.noreply.github.com>
2026-05-19 08:15:53 +0700

    Update AuditJob.php
    remove count check audit trails

--- src/Jobs/AuditJob.php
+++ src/Jobs/AuditJob.php
@@ -32,44 +32,42 @@ class AuditJob implements ShouldQueue, ShouldDispatchAfterCommit
      */
     public function handle(): void
     {
-        if ($this->audit->trails->count() > 0) {
-            DB::beginTransaction();
+        DB::beginTransaction();
 
-            /** @var Audit $auditModel */
-            $auditModel =
-                config( 'laravel_audit.audit_model')::query()->create([
-                    "message" => $this->audit->message,
-                    "action" => $this->audit->action,
-                    "ip_address" => $this->audit->ipAddress,
-                    "endpoint" => $this->audit->endpoint,
-                    "method" => $this->audit->method,
-                    "user_agent" => $this->audit->userAgent,
-                    "user_request" => json_encode($this->audit->userRequest, JSON_THROW_ON_ERROR),
-                    "actor_table" => $this->audit->actorTable,
-                    "actor_id" => $this->audit->actorId,
-                    "actor_name" => $this->audit->actorName,
-                    "actor_email" => $this->audit->actorEmail,
-                    "actor_phone" => $this->audit->actorPhone,
-                    "entry_object_table" => $this->audit->entryObjectTable,
-                    "entry_object_id" => $this->audit->entryObjectId,
-                    "tag" => json_encode($this->audit->tag, JSON_THROW_ON_ERROR),
-                    "additional" => json_encode($this->audit->additional, JSON_THROW_ON_ERROR),
-                    "app_name" => $this->audit->appName,
-                ]);
+        /** @var Audit $auditModel */
+        $auditModel =
+            config( 'laravel_audit.audit_model')::query()->create([
+                "message" => $this->audit->message,
+                "action" => $this->audit->action,
+                "ip_address" => $this->audit->ipAddress,
+                "endpoint" => $this->audit->endpoint,
+                "method" => $this->audit->method,
+                "user_agent" => $this->audit->userAgent,
+                "user_request" => json_encode($this->audit->userRequest, JSON_THROW_ON_ERROR),
+                "actor_table" => $this->audit->actorTable,
+                "actor_id" => $this->audit->actorId,
+                "actor_name" => $this->audit->actorName,
+                "actor_email" => $this->audit->actorEmail,
+                "actor_phone" => $this->audit->actorPhone,
+                "entry_object_table" => $this->audit->entryObjectTable,
+                "entry_object_id" => $this->audit->entryObjectId,
+                "tag" => json_encode($this->audit->tag, JSON_THROW_ON_ERROR),
+                "additional" => json_encode($this->audit->additional, JSON_THROW_ON_ERROR),
+                "app_name" => $this->audit->appName,
+            ]);
 
 
-            foreach ($this->audit->trails as $trail) {
-                $auditModel->audit_trails()->create([
-                    "object_table" => $trail["object_table"],
-                    "object_id" => $trail["object_id"] ?? null,
-                    "before" => $trail["before"],
-                    "after" => $trail["after"],
-                    "tag" => $trail["tag"],
-                    "additional" => $trail["additional"],
-                ]);
-            }
-
-            DB::commit();
+        foreach ($this->audit->trails as $trail) {
+            $auditModel->audit_trails()->create([
+                "object_table" => $trail["object_table"],
+                "object_id" => $trail["object_id"] ?? null,
+                "before" => $trail["before"],
+                "after" => $trail["after"],
+                "tag" => $trail["tag"],
+                "additional" => $trail["additional"],
+            ]);
         }
+
+        DB::commit();
     }
 }
